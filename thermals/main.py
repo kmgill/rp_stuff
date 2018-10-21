bd55a34f4faf5d557f8d01f8d58fc3ec995e1add
Kevin M. Gill <kmsmgill@gmail.com>
2018-10-21 16:57:05 +0000

    Added a scrolling line plot of mean values to the PiOLED display

--- thermals/main.py
+++ thermals/main.py
@@ -8,7 +8,7 @@ from scipy.misc import imresize
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
+import sys
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
@@ -22,6 +22,8 @@ colors = np.array([
 	[255, 0, 0],
 	[79, 0, 0]
 ])
+
+means = None
 
 
 class PiOLED:
@@ -38,6 +40,10 @@ class PiOLED:
 		self.draw = ImageDraw.Draw(self.image)
 		self.font = ImageFont.load_default()
 		self.clear()
+		
+		#y = self.height / 2
+		#self.draw.line(((self.width/2, y), (self.width, y)), fill=255, width=1)
+		#self.show()
 
 	def clear(self, disp=True):
 		self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
@@ -51,6 +57,26 @@ class PiOLED:
 
 	def text(self, str, xy):
 		self.draw.text(xy, str, font=self.font, fill=255)
+
+	def line(self, xy):
+		self.draw.line(xy, fill=244, width=1)
+
+def add_mean(m):
+	for i in range(len(means) - 1):
+		means[i] = means[i+1]
+	means[len(means)-1] = m
+
+
+def draw_means_line():
+	min = np.nanmin(means)
+	max = np.nanmax(means)
+	for i in range(len(means) - 1):
+		x = int(led_disp.width / 2) + i
+		m0 = means[i]
+		m1 = means[i+1]
+		y0 = (1.0 - ((m0 - min) / (max - min))) * led_disp.height
+		y1 = (1.0 - ((m1 - min) / (max - min))) * led_disp.height
+		led_disp.line(((x, y0), (x+1, y1)))
 
 
 """
@@ -86,10 +112,12 @@ def display_to_pioled(amg_pixels):
 	min = amg_pixels.min()
 	max = amg_pixels.max()
 	mean = amg_pixels.mean()
+	add_mean(mean)
 	led_disp.clear(disp=False)
-	led_disp.text("Min: %.3f"%min, (0, 2))
-	led_disp.text("Max: %.3f"%max, (0, 10))
-	led_disp.text("Mean: %.3f"%mean, (0, 18))
+	led_disp.text("Min: %.1f"%min, (0, 2))
+	led_disp.text("Max: %.1f"%max, (0, 10))
+	led_disp.text("Mean: %.1f"%mean, (0, 18))
+	draw_means_line()
 	led_disp.show()
 
 
@@ -108,8 +136,10 @@ if __name__ == "__main__":
 	amg = adafruit_amg88xx.AMG88XX(i2c)
 
 	led_disp = PiOLED()
-	led_disp.text("Hello There", (0, 2))
-	led_disp.show()
+	
+	mean_list_len = int(led_disp.width / 2)
+	means = np.zeros((mean_list_len,))
+	means[:] = np.nan
 
 	try:
 		loop_display(amg)
